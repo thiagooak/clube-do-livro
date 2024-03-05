51cf398280485dc6eb4ef6efd7b0c40e3287770b
Thiago Carvalho <thiago.oak@gmail.com>
2024-03-05 16:32:51 -0300

    Second meeting content

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -130,63 +130,95 @@ export function Two() {
           céu e sobre todos os animais que rastejam sobre a terra.
         </ChapterQuote>
         <QuoteContainer>
-          <Quote author="Lorem Ipsum">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            a elit tellus. Morbi semper placerat sapien, mattis dapibus diam
-            pulvinar ac. Ut et ornare sapien.
+          <Quote author="(p56)">
+            Para que haja crescimento espiritual, existe uma Palavra divina a
+            ser obedecida. Para que haja desenvolvimento cultural e criativo,
+            existe o trabalho físico de cuidar do jardim e a tarefa de dar nomes
+            aos animais, que envolve esforço mental e discernimento.
           </Quote>
 
           <div>
             <Image
-              src="/piret-ilver-98MbUldcDJY-unsplash.jpg"
+              src="/rajesh-ram-HOOKgN_zIY8-unsplash.jpg"
               width={400}
               height={400}
               style={{ objectFit: "cover" }}
               className="px-4 py-3 mr-2"
-              alt="Uma balança antiga"
+              alt="Uma mulher e um homem trabalhando num campo com dois bovinos"
             />
-            <Quote author="(p00)" variant="secondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse a elit tellus. Morbi semper placerat sapien, mattis
-              dapibus diam pulvinar ac. Ut et ornare sapien.
+            <Quote author="(p59)" variant="secondary">
+              Temos de ser jardineiros que adotam uma postura ativa em suas
+              tarefas. Jardineiros não deixam a terra como a encontram. Eles a
+              retrabalham para torná-la mais frutífera, para extrair os
+              potenciais de crescimento e desenvolvimento do solo. Os
+              jardineiros escavam o solo e trabalham nele com um objetivo em
+              mente: dispor a matéria-prima do jardim de modo que produza
+              alimento, flores e beleza. E esse é o padrão para todo e qualquer
+              trabalho. Ele é criativo e assertivo. Dedica-se a trabalhar a
+              matéria-prima da criação de Deus de tal forma que ajude o mundo em
+              geral e as pessoas em particular a crescer e florescer.
             </Quote>
           </div>
         </QuoteContainer>
       </Chapter>
       <Divider />
       <Chapter name="4. O trabalho como serviço">
-        <ChapterQuote author={"Gênesis 1.26,27"}>
+        <ChapterQuote author={"1 Coríntios 7.17"}>
           Somente viva cada um como o Senhor lhe determinou, cada um como Deus o
           chamou. É isso que ordeno em todas as igrejas.
         </ChapterQuote>
 
         <QuoteContainer>
           <Image
-            src="/peter-burdon-FmUgZxCIJhc-unsplash.jpg"
+            src="/arron-choi-fYD54gVXFGM-unsplash.jpg"
             width={400}
             height={400}
             style={{ objectFit: "cover" }}
             className="px-4 py-3 mr-2"
-            alt="5 trabalhadores em uma mina empurrando carros de carga em um trilho"
+            alt="Trabalhador usando marreta em uma construção"
           />
 
           <div>
-            <Quote author="(p00)">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse a elit tellus. Morbi semper placerat sapien, mattis
-              dapibus diam pulvinar ac. Ut et ornare sapien.
+            <Quote author="(p71)">
+              Até mesmo seus trabalhos aparentemente seculares são um culto a
+              Deus e um ato de obediência que agrada a Deus - Martinho Lutero
             </Quote>
 
-            <Quote author="(p00)" variant="secondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse a elit tellus. Morbi semper placerat sapien, mattis
-              dapibus diam pulvinar ac. Ut et ornare sapien.
+            <Quote author="(p65)" variant="secondary">
+              Nosso trabalho diário só é um chamado se for reconhecido como
+              designação de Deus para servirmos ao próximo. É exatamente assim
+              que a Bíblia nos ensina a ver o trabalho.
+            </Quote>
+
+            <Quote author="(p76)" variant="secondary">
+              “Os cristãos não precisam exercer o ministério diretamente ou
+              trabalhar para organizações de caridade para amar o semelhante por
+              meio do trabalho.”
             </Quote>
           </div>
         </QuoteContainer>
       </Chapter>
       <Divider />
-      <Exercise name={"Definindo trabalho"}>TBD</Exercise>
+      <Exercise name={"Influências"}>
+        <Image
+          src="/influencia.jpg"
+          width={400}
+          height={400}
+          style={{ objectFit: "cover" }}
+          className="my-2 mx-auto border rounded-md"
+          alt="Trabalhador usando marreta em uma construção"
+        />
+
+        <StorageTextArea question="Complete a frase: Eu cresci em uma fazenda/cidade grande/cidade pequena, e por isso aprendi que o trabalho ..." />
+
+        <StorageTextArea question="Complete a frase: Minha Mãe/Pai/Cuidadora trabalhava como ... e por isso aprendi que o trabalho ..." />
+
+        <StorageTextArea question="Complete a frase: Meu pior/melhor trabalho foi/é ..." />
+
+        <StorageTextArea question="Complete a frase: No meu círculo familiar e de amigos o trabalho era visto como ..." />
+
+        <StorageTextArea question="Complete a frase: Quando eu tinha 10 anos de idade eu queria ser ... porque ..." />
+      </Exercise>
     </>
   );
 }
